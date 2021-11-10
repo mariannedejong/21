57694866b6103b2ec23249c57e06fb2d74b6128d
mariannedejong <89845904+mariannedejong@users.noreply.github.com>
2021-11-11 00:15:50 +0100

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 20; i++) {
+    console.log(`Is ${i} even or odd?`);
+
+    if(i % 2 == 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
+}
---
+++ grade-assigner.js
@@ -0,0 +1,18 @@
+function assignGrade(score) {
+    if (score > 90) {
+      return "A";
+    } else if (score > 80) {
+      return "B";
+    } else if (score > 70) {
+      return "C";
+    } else if (score > 65) {
+      return "D";
+    } else {
+      return "E";
+    }
+}
+
+for (score=60; score<100; score++){
+    let grade = assignGrade(score);
+    console.log ("For scoring ", score, " points, you get a ", grade);
+}
